d6429b7fa15b6f0fb0d1ca3f16c19e2d63bb3ae0
wackybackyy <frustraautem@yandex.ru>
2026-04-20 20:57:57 +0300

    ST-4 done

---
+++ BugPro/Program.cs
@@ -0,0 +1,92 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public class Bug {
+    public enum State {
+        New,
+        Triaged,
+        NeedInfo,
+        InProgress,
+        Resolved,
+        Closed,
+        Rejected,
+        Duplicate,
+        NotReproducible
+    }
+
+    public enum Trigger {
+        Triaging,
+        RequestInfo,
+        ProvideInfo,
+        StartFix,
+        Resolve,
+        Close,
+        Reopen,
+        Reject,
+        MarkDuplicate,
+        MarkNotReproducible
+    }
+
+    private readonly StateMachine<State, Trigger> machine;
+
+    public Bug() {
+        machine = new StateMachine<State, Trigger>(State.New);
+
+        machine.Configure(State.New)
+            .Permit(Trigger.Triaging, State.Triaged);
+
+        machine.Configure(State.Triaged)
+            .Permit(Trigger.RequestInfo, State.NeedInfo)
+            .Permit(Trigger.StartFix, State.InProgress)
+            .Permit(Trigger.Reject, State.Rejected)
+            .Permit(Trigger.MarkDuplicate, State.Duplicate)
+            .Permit(Trigger.MarkNotReproducible, State.NotReproducible);
+
+        machine.Configure(State.NeedInfo)
+            .Permit(Trigger.ProvideInfo, State.Triaged)
+            .Permit(Trigger.Reject, State.Rejected);
+
+        machine.Configure(State.InProgress)
+            .Permit(Trigger.Resolve, State.Resolved)
+            .Permit(Trigger.RequestInfo, State.NeedInfo);
+
+        machine.Configure(State.Resolved)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Reopen, State.InProgress);
+
+        machine.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.InProgress);
+
+        machine.Configure(State.Rejected)
+            .Permit(Trigger.Reopen, State.Triaged);
+
+        machine.Configure(State.Duplicate)
+            .Permit(Trigger.Reopen, State.Triaged);
+
+        machine.Configure(State.NotReproducible)
+            .Permit(Trigger.Reopen, State.Triaged);
+    }
+
+    public State CurrentState => machine.State;
+
+    public void Fire(Trigger trigger) {
+        machine.Fire(trigger);
+    }
+}
+
+internal class Program {
+    private static void Main(string[] args) {
+        var bug = new Bug();
+
+        Console.WriteLine($"Initial state: {bug.CurrentState}");
+        bug.Fire(Bug.Trigger.Triaging);
+        Console.WriteLine($"After triage: {bug.CurrentState}");
+        bug.Fire(Bug.Trigger.StartFix);
+        Console.WriteLine($"After start fix: {bug.CurrentState}");
+        bug.Fire(Bug.Trigger.Resolve);
+        Console.WriteLine($"After resolve: {bug.CurrentState}");
+        bug.Fire(Bug.Trigger.Close);
+        Console.WriteLine($"After close: {bug.CurrentState}");
+    }
+}
---
+++ BugTests/Test1.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stateless;
+using BugPro;
+
+namespace BugTests;
+
+[TestClass]
+public class UnitTest1 {
+    private Bug bug = null!;
+
+    [TestInitialize]
+    public void Setup() {
+        bug = new Bug();
+    }
+
+    [TestMethod]
+    public void InitialState_ShouldBeNew() {
+        Assert.AreEqual(Bug.State.New, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void New_To_Triaged() {
+        bug.Fire(Bug.Trigger.Triaging);
+        Assert.AreEqual(Bug.State.Triaged, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Triaged_To_NeedInfo() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        Assert.AreEqual(Bug.State.NeedInfo, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void NeedInfo_To_Triaged() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        bug.Fire(Bug.Trigger.ProvideInfo);
+        Assert.AreEqual(Bug.State.Triaged, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Triaged_To_InProgress() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void InProgress_To_Resolved() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        Assert.AreEqual(Bug.State.Resolved, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Resolved_To_Closed() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        bug.Fire(Bug.Trigger.Close);
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Resolved_To_InProgress_ByReopen() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        bug.Fire(Bug.Trigger.Reopen);
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Closed_To_InProgress_ByReopen() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        bug.Fire(Bug.Trigger.Close);
+        bug.Fire(Bug.Trigger.Reopen);
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Triaged_To_Rejected() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.Reject);
+        Assert.AreEqual(Bug.State.Rejected, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Rejected_To_Triaged_ByReopen() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.Reject);
+        bug.Fire(Bug.Trigger.Reopen);
+        Assert.AreEqual(Bug.State.Triaged, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Triaged_To_Duplicate() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkDuplicate);
+        Assert.AreEqual(Bug.State.Duplicate, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Duplicate_To_Triaged_ByReopen() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkDuplicate);
+        bug.Fire(Bug.Trigger.Reopen);
+        Assert.AreEqual(Bug.State.Triaged, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Triaged_To_NotReproducible() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkNotReproducible);
+        Assert.AreEqual(Bug.State.NotReproducible, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void NotReproducible_To_Triaged_ByReopen() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkNotReproducible);
+        bug.Fire(Bug.Trigger.Reopen);
+        Assert.AreEqual(Bug.State.Triaged, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void InProgress_To_NeedInfo() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        Assert.AreEqual(Bug.State.NeedInfo, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void FullHappyPath_ShouldEndInClosed() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        bug.Fire(Bug.Trigger.Close);
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void NeedInfo_CanBeRejected() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        bug.Fire(Bug.Trigger.Reject);
+        Assert.AreEqual(Bug.State.Rejected, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReopenAfterDuplicate_ThenStartFix() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkDuplicate);
+        bug.Fire(Bug.Trigger.Reopen);
+        bug.Fire(Bug.Trigger.StartFix);
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReopenAfterNotReproducible_ThenRequestInfo() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkNotReproducible);
+        bug.Fire(Bug.Trigger.Reopen);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        Assert.AreEqual(Bug.State.NeedInfo, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void New_CannotBeClosed_Directly() {
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Close);
+        });
+    }
+
+    [TestMethod]
+    public void New_CannotStartFix_Directly() {
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.StartFix);
+        });
+    }
+
+    [TestMethod]
+    public void Triaged_CannotBeClosed_Directly() {
+        bug.Fire(Bug.Trigger.Triaging);
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Close);
+        });
+    }
+
+    [TestMethod]
+    public void NeedInfo_CannotBeClosed_Directly() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.RequestInfo);
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Close);
+        });
+    }
+
+    [TestMethod]
+    public void InProgress_CannotBeClosed_Directly() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Close);
+        });
+    }
+
+    [TestMethod]
+    public void Closed_CannotBeClosedAgain() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+        bug.Fire(Bug.Trigger.Close);
+
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Close);
+        });
+    }
+
+    [TestMethod]
+    public void Rejected_CannotResolve() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.Reject);
+
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Resolve);
+        });
+    }
+
+    [TestMethod]
+    public void Duplicate_CannotProvideInfo() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkDuplicate);
+
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.ProvideInfo);
+        });
+    }
+
+    [TestMethod]
+    public void NotReproducible_CannotStartFix() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.MarkNotReproducible);
+
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.StartFix);
+        });
+    }
+
+    [TestMethod]
+    public void Resolved_CannotBeTriagedAgain() {
+        bug.Fire(Bug.Trigger.Triaging);
+        bug.Fire(Bug.Trigger.StartFix);
+        bug.Fire(Bug.Trigger.Resolve);
+
+        Assert.ThrowsException<InvalidOperationException>(() => {
+            bug.Fire(Bug.Trigger.Triaging);
+        });
+    }
+}
